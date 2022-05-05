a57d014b5002866d1ebc62724a80e8d10809cf9c
Sourav Kumar Gupta <89971045+SouravKrGupta@users.noreply.github.com>
2022-05-05 21:44:53 +0530

    my first mini project in my college.
    my first mini project in my college.

---
+++ questions.js
@@ -0,0 +1,129 @@
+let questions = [
+  
+   
+    {
+    
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    
+    numb: 2,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+  
+
+    numb: 2,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+  
+    numb: 3,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+   {
+
+    numb: 4,
+        question: "A page designed in HTML is called _____",
+   answer: "Web Page",
+    options: [
+      " Application",
+     "Cover page",
+       "Front-end",
+     "Web Page"
+   ]
+   },
+   {
+
+    numb: 6,
+    question: " Choose the correct HTML tag for a large title.",
+    answer: "H1",
+    options: [
+      "H1",
+      "Heading",
+      "H3",
+      "H6"
+    ]
+  },
+  {
+
+    numb: 7,
+    question: " Which tag is used to lists the items with bulle lists the items with bullets?",
+    answer: "ul",
+    options: [
+      "list",
+      "ul",
+      "ol",
+      "br"
+    ]
+  },
+  {
+
+    numb: 8,
+    question: "HTML tags are used to describe document____",
+    answer: "Content",
+    options: [
+      "Content",
+      "Definition",
+      "Language",
+      "none of these"
+    ]
+  },
+  {
+
+    numb: 9,
+    question: "HTML was first proposed in year?",
+    answer: "1990",
+    options: [
+      "2001",
+      "1990",
+      "1995",
+      "2000"
+    ]
+  },
+  
+  {
+
+    numb: 10,
+    question: "What does HTML stand for?",
+    answer:"Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Multiple Language",
+      "Hyper Text Markup Language",
+      "Hyper Tool Multi Language"
+    ]
+   },
+   
+ 
+];
